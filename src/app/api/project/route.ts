8e2f595da085fbb234509890c1fbcf18ec0ed1fa
Vova Puchkov <vova.puchkoff@gmail.com>
2023-07-18 16:30:28 +0300

    fix: (/api/project): get cookie

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse, type NextRequest } from 'next/server'
+import { cookies } from "next/headers";
 import { db } from "~lib/db";
 import { validateJWT } from "~lib/jwt";
 import { COOKIE_NAME } from '~env';
 
-export default async function handler(req: NextRequest, res) {
-  const user = await validateJWT(req.cookies[COOKIE_NAME]);
+export async function POST(req: NextRequest) {
+  const user = await validateJWT(cookies().get(COOKIE_NAME).value);
   const payload = await req.json();
   const { name } = payload;
 
